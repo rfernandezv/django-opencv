3b932ee5609ba38bb9cfc35ce233268e53c19997
rfernandezv <richar.fernandezv@gmail.com>
2019-08-05 23:45:29 -0500

    subir2

--- video/views.py
+++ video/views.py
@@ -5,34 +5,62 @@ from django.contrib import messages
 from django.views.decorators import gzip
 import cv2
 import time
+import base64
+
+imagen_base64 = None
+imagen_read = None
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html', {'inicia_captura':False})
+    return render(request,'index.html', {'inicia_captura':False, "imagen_capturada":None})
 
 def iniciar_video(request):
-    return render(request,'index.html', {'inicia_captura':True})
+    return render(request,'index.html', {'inicia_captura':True, "imagen_capturada":imagen_base64})
 
 def detener_video(request):
-    return render(request,'index.html', {'inicia_captura':False})
+    if imagen_read is not None:
+        cv2.imwrite("media/opencv/test.jpg", imagen_read)
+    
+    return render(request,'index.html', {'inicia_captura':False, "imagen_capturada":imagen_base64})
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
+        self.video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
     def __del__(self):
+        print("del")
         self.video.release()
+        cv2.destroyAllWindows()
 
     def get_frame(self):
-        ret,image = self.video.read()
-        #image = cv2.Canny(image,100,200) 
-        ret,jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+        try:
+            _,image = self.video.read()                
+            _,jpeg = cv2.imencode('.jpg', image)            
 
+            global imagen_base64
+            global imagen_read 
+            frame_b64 = base64.b64encode(jpeg)
+            imagen_base64 = frame_b64.decode("utf-8")
+            imagen_read = image
+
+            return jpeg.tobytes()
+        except KeyboardInterrupt:
+            self.video.release()
+            cv2.destroyAllWindows()
+            exit(0)
+        except Exception:
+            self.video.release()
+            cv2.destroyAllWindows()
+            exit(0)
+            
 def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield(b'--frame\r\n'
-        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    try:
+        while True:
+            frame = camera.get_frame()
+            yield(b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    except:
+        print("aborted 2")
 
 @gzip.gzip_page
 def live_video(request):
